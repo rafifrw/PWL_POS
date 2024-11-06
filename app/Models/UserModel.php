140bdc09d332979ceea7e2638ae67867779bc911
rafifrw <rafifrw@gmail.com>
2024-11-06 09:00:33 +0700

    JS 10 Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,41 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Arr;
-use App\Models\LevelModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo; 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['username', 'password', 'nama', 'foto', 'level_id', 'created_at', 'updated_at'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'profile_image', 'created_at', 'updated_at'];
 
-    protected $hidden = ['password'];
+    protected $hidden = ['password']; // Tidak ditampilkan saat select
+    protected $casts = ['password' => 'hashed']; // Password akan di-hash secara otomatis
 
-    protected $casts = ['password' => 'hashed'];
-
-    public function level(): BelongsTo{
+    public function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-    
+
     public function getRoleName(): string
     {
         return $this->level->level_nama;
-    }
-
-    public function hasRole($role): bool
+    }    
+    
+    public function hasRole($role) : bool
     {
         return $this->level->level_kode == $role;
     }
-    
+
     public function getRole()
     {
         return $this->level->level_kode;
-    } 
+    }
 }
